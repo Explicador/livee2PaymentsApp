b01a9a0969bd403dfedeab3f5247d12d4bf1158d
Jose Seie <joseeugenioseie@hotmail.com>
2022-04-14 18:04:58 +0200

    ui: layout do site

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,35 +8,47 @@
           round
           icon="menu"
           aria-label="Menu"
-          @click="toggleLeftDrawer"
+          @click="isDrawerOpen = !isDrawerOpen"
         />
 
         <q-toolbar-title>
-          Quasar App
+          e2Payments Live Tests
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>e2Payments v1.0.0</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      v-model="isDrawerOpen"
       show-if-above
-      bordered
+      :width="300"
+      :breakpoint="400"
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+      <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+        <q-list padding>
+          <q-item to="/" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="home" />
+            </q-item-section>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+            <q-item-section>
+              Inicio
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+      </q-scroll-area>
+
+      <q-img class="absolute-top" src="https://explicador.co.mz/wp-content/uploads/2021/07/lagos-techie-IgUR1iX0mqM-unsplash-1-scaled.jpg" style="height: 150px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://explicador.co.mz/wp-content/uploads/2021/11/app-icon.png">
+          </q-avatar>
+          <div class="text-weight-bold">Explicador Inc</div>
+          <div>@e2Payments</div>
+        </div>
+      </q-img>
     </q-drawer>
 
     <q-page-container>
@@ -46,72 +58,13 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-import { defineComponent, ref } from 'vue'
-
-export default defineComponent({
+export default {
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
+  data () {
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      isDrawerOpen: false
     }
   }
-})
+}
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,88 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="q-pa-xl">
+    <div class="row">
+
+      <div class="col-8">
+        <div class="text-center">
+          <img src="https://user-images.githubusercontent.com/18400142/132318330-b8536515-d0d3-44ba-817e-ee2d269722f6.png"
+               alt=""
+               style="width: 100px;height: 100px"
+          >
+        </div>
+
+        <q-form
+          @submit="onSubmit"
+          class="q-gutter-md"
+        >
+          <q-input
+            filled
+            v-model="payload.amount"
+            label="Insira o valor a pagar *"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Por favor, preencha este campo']"
+          />
+
+          <q-input
+            filled
+            type="number"
+            v-model="payload.phone"
+            label="Numero com MPesa 84/85"
+            lazy-rules
+            :rules="[
+              val => val !== null && val !== '' || 'Por favor, preencha este campos',
+              val => val > 0 && val.toString().length === 9 || 'Insira um numero valido'
+            ]"
+          />
+
+          <q-select
+            filled
+            v-model="payload.reference"
+            :options="options"
+            label="Indique a referencia"
+            lazy-rules
+            :rules="[
+              val => val !== null && val !== 'Selecione' || 'Por favor, selecione alguma opcao',
+            ]"
+          />
+
+          <div class="text-right">
+            <q-btn label="Submit" type="submit" color="primary" style="width: 180px;height: 40px"/>
+          </div>
+        </q-form>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  name: 'Index',
+  data () {
+    return {
+      payload: {
+        amount: null,
+        phone: null,
+        reference: null,
+      },
+      options: [
+        'Selecione', 'Live', 'TestarPagamento', 'PagarCurso', 'DoarValor'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+
+
+      this.$q.notify({
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'check_circle',
+        message: 'Parabens, Formulario Submetido...'
+      })
+
+    },
+
+  }
+}
 </script>
